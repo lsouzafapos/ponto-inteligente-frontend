102fdc9febcac9dc1f9b0ec53633ecd97b8393dc
Leonardo Souza Faria <lsouzafapos@gmail.com>
2022-07-25 21:35:57 -0300

    Incluindo model de PJ

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { CadastroPj } from '../../models';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -38,7 +39,8 @@ export class CadastrarPjComponent implements OnInit {
     if(this.form.invalid){
       return;
     }
-    alert(JSON.stringify(this.form.value));
+    const cadastroPj: CadastroPj = this.form.value;
+    alert(JSON.stringify(cadastroPj));
   }
 
 }
