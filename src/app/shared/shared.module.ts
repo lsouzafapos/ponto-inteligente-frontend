b4db4516f3c3fa2e2e2aafe425b179ed327db772
Leonardo Souza Faria <lsouzafapos@gmail.com>
2022-09-30 07:45:50 -0300

    Incluindo pipe para data

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MascaraDirective } from './directives/mascara.directive';
 import { PtBrMatPaginatorIntl } from './pt-br-mat-paginator-intl';
 import { TipoPipe } from './pipes/tipo.pipe';
+import { DataPipe } from './pipes/data.pipe';
 
 
 
 @NgModule({
   declarations: [
     MascaraDirective,
-    TipoPipe
+    TipoPipe,
+    DataPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     MascaraDirective,
-    TipoPipe
+    TipoPipe,
+    DataPipe
   ],
   providers: [
     PtBrMatPaginatorIntl
